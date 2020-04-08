bbb59e83cd1e50bf44b9c4489cc651312ff4cf0c
Mark Tiddy <marktiddy@gmail.com>
2020-04-08 09:39:10 +0100

    Updating UI

--- src/App.js
+++ src/App.js
@@ -5,11 +5,25 @@ import "./App.css";
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 
+//import API
+import { fetchData } from "./api";
+
 class App extends React.Component {
+  //Set some state
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const { data } = await fetchData();
+    this.setState({ data: data });
+  }
+
   render() {
+    const data = this.state.data;
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import styles from "./Cards.module.css";
 
-const Cards = () => {
-  return <h2>Cards</h2>;
+const Cards = ({ data: { confirmed, deaths, recovered, lastUpdate } }) => {
+  if (!confirmed) {
+    return "Loading...";
+  }
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">{confirmed.value}</Typography>
+            <Typography color="textSecondary">
+              Last Updated: {lastUpdate}
+            </Typography>
+            <Typography variant="body2">
+              Number of active cases of COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Recovered
+            </Typography>
+            <Typography variant="h5">{recovered.value}</Typography>
+            <Typography color="textSecondary">
+              Last Updated: {lastUpdate}
+            </Typography>
+            <Typography variant="body2">
+              Number of recoveries from COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="body2">
+              Number of deaths from COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Cards;
