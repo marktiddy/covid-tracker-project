33f2d1b8767013e2aad358056cd2ef9c98482853
Mark Tiddy <marktiddy@gmail.com>
2020-04-09 14:22:49 +0100

    Updates

---
+++ precache-manifest.a016c4cd0c788270e3a0354fab777ecd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5bb59e8dc755761b9d1f5159b69590c",
+    "url": "/covid-tracker-project/index.html"
+  },
+  {
+    "revision": "1c49c231108a01441ec4",
+    "url": "/covid-tracker-project/static/css/main.2607568e.chunk.css"
+  },
+  {
+    "revision": "89cccc22b8711934cd43",
+    "url": "/covid-tracker-project/static/js/2.4f198110.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/covid-tracker-project/static/js/2.4f198110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c49c231108a01441ec4",
+    "url": "/covid-tracker-project/static/js/main.376f2107.chunk.js"
+  },
+  {
+    "revision": "90cccb3daff80bfdd9a8",
+    "url": "/covid-tracker-project/static/js/runtime-main.818ad5d5.js"
+  }
+]);
